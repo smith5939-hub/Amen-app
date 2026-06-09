e71c311126a4d6176cce37cdd695dd9973046bbb
smith5939-hub <smith.5939@gmail.com>
2026-06-09 15:29:45 +0000

    Update service worker to Firebase 10.12.0

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyChaWymkd5XlX5pvTWSigwUof11SXM5DWs",
