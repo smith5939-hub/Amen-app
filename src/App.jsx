fd5fde3ed72efd140b0e1d03b641eecd7c2566c4
smith5939-hub <smith.5939@gmail.com>
2026-06-12 02:00:44 +0000

    use praying hands icon for prayers tab

--- src/App.jsx
+++ src/App.jsx
@@ -1831,14 +1831,15 @@ function NavIcon({ type, active }) {
 
   if (type === "prayers") {
     return (
-      <svg {...common}>
-        <path d="M8 6h13" />
-        <path d="M8 12h13" />
-        <path d="M8 18h13" />
-        <path d="M3 6h.01" />
-        <path d="M3 12h.01" />
-        <path d="M3 18h.01" />
-      </svg>
+      <PiHandsPrayingBold
+        aria-hidden="true"
+        style={{
+          width: 24,
+          height: 24,
+          color: active ? T.sageDark : T.inkLight,
+          display: "block"
+        }}
+      />
     );
   }
 
