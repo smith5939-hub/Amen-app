9de715ec96f9f20d0f7167e208a88ac89e1700cb
Adam Smith <smith.5939@gmail.com>
2026-06-22 21:24:11 -0400

    Fix Android native Google Sign-In - implement SocialLogin interface

--- android/app/src/main/java/com/liftprayer/app/MainActivity.java
+++ android/app/src/main/java/com/liftprayer/app/MainActivity.java
@@ -1,5 +1,12 @@
 package com.liftprayer.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
+    @Override
+    public void IHaveModifiedTheMainActivityForTheUseWithSocialLoginPlugin() {
+        // Required by SocialLogin plugin
+    }
+}
